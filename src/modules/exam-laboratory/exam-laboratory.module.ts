571aa988523861b8520aca2d9e32ec618d73cfd8
Rodrigo Alves <rodrigoalves009@gmail.com>
2021-10-16 17:12:02 -0400

    fix: missing shared module on exam and providers and controllers for exam laboratory

--- src/modules/exam-laboratory/exam-laboratory.module.ts
+++ src/modules/exam-laboratory/exam-laboratory.module.ts
@@ -1,4 +1,20 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ExamModule } from '../exam/exam.module'
+import { LaboratoryModule } from '../laboratory/laboratory.module'
+import { SharedModule } from '../shared/shared.module'
+import { ExamLaboratoryController } from './exam-laboratory.controller'
+import { ExamLaboratory } from './exam-laboratory.entity'
+import { ExamLaboratoryService } from './exam-laboratory.service'
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([ExamLaboratory]),
+    SharedModule,
+    ExamModule,
+    LaboratoryModule
+  ],
+  controllers: [ExamLaboratoryController],
+  providers: [ExamLaboratoryService]
+})
 export class ExamLaboratoryModule {}
--- src/modules/exam/exam.module.ts
+++ src/modules/exam/exam.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { SharedModule } from '../shared/shared.module'
 import { ExamController } from './exam.controller'
 import { Exam } from './exam.entity'
 import { ExamService } from './exam.service'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Exam])],
+  imports: [TypeOrmModule.forFeature([Exam]), SharedModule],
   controllers: [ExamController],
   providers: [ExamService]
 })
