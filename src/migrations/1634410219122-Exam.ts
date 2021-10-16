26db2ac47c658ee449dbe5a3803b3ce57c365d7b
Rodrigo Alves <rodrigoalves009@gmail.com>
2021-10-16 14:53:34 -0400

    feat: added exam migration

---
+++ src/migrations/1634410219122-Exam.ts
@@ -0,0 +1,52 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class Exam1634410219122 implements MigrationInterface {
+  public async up (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(new Table({
+      name: 'exams',
+      columns: [
+        {
+          name: 'id',
+          type: 'int',
+          isPrimary: true,
+          generationStrategy: 'increment'
+        },
+        {
+          name: 'name',
+          type: 'varchar'
+        },
+        {
+          name: 'type',
+          type: 'varchar',
+          enum: ['clinical', 'image']
+        },
+        {
+          name: 'status',
+          type: 'varchar',
+          enum: ['active', 'inactive'],
+          default: "'active'"
+        },
+        {
+          name: 'created_at',
+          type: 'timestamp',
+          default: 'now()'
+        },
+        {
+          name: 'updated_at',
+          type: 'timestamp',
+          default: 'now()',
+          onUpdate: 'now()'
+        },
+        {
+          name: 'deleted_at',
+          type: 'timestamp',
+          isNullable: true
+        }
+      ]
+    }))
+  }
+
+  public async down (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('exams')
+  }
+}
