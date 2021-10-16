2629ee579b58ec4a1951e5341905db7595d00bf8
Rodrigo Alves <rodrigoalves009@gmail.com>
2021-10-16 15:48:43 -0400

    feat: added some exports to shared.module

--- src/modules/shared/shared.module.ts
+++ src/modules/shared/shared.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common'
+import { FilterQueryBuilder } from './filter-query.builder'
 import { IsLikeSearch } from './is-like-search'
 
 @Module({
-  providers: [IsLikeSearch],
-  exports: [IsLikeSearch]
+  providers: [IsLikeSearch, FilterQueryBuilder],
+  exports: [IsLikeSearch, FilterQueryBuilder]
 })
 export class SharedModule {}
