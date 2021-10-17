e9976f217ebace72bb8d5652795072a573bdebf7
Rodrigo Alves <rodrigoalves009@gmail.com>
2021-10-17 10:23:41 -0400

    feat: added todo for all exam integration cases

---
+++ src/modules/exam/tests/exam.integration.spec.ts
@@ -0,0 +1,27 @@
+describe('exam integration tests', () => {
+  describe('GET /exams', () => {
+    it.todo('should return a paginated list of exams')
+  })
+
+  describe('GET /exams/:id', () => {
+    it.todo('should return 404 when exam doesnt exists')
+
+    it.todo('should return exam')
+  })
+
+  describe('POST /exams', () => {
+    it.todo('should return created exam')
+  })
+
+  describe('PUT /exams/:id', () => {
+    it.todo('should return 404 when user try to update an exam that doesnt exists')
+
+    it.todo('should return updated exam')
+  })
+
+  describe('PUT /exams/:id', () => {
+    it.todo('should return 404 when try to delete an exam that doesnt exists')
+
+    it.todo('should delete exam from database')
+  })
+})
